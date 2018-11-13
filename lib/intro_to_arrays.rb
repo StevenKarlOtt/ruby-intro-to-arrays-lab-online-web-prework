c20f2009a4503ca800731f73bf88da8fab3a1333
Steven Ott <stevenkott@icloud.com>
2018-11-13 17:43:29 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -4,3 +4,17 @@ end
 
 def array_with_two_elements
   ["Squids","Rats"]
+end
+
+def first_element(array)
+  array[0]
+end
+
+def third_element(array)
+  array[2]
+end
+
+def last_element(array)
+  array[-1]
+end
+  
